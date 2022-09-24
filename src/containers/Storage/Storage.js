1ed4153be7e00abe081093cd7cfaa071354864e2
Dmytro <dmytrobuchkovsky@gmail.com>
2022-09-24 17:20:34 +0300

    fix Options Bar

--- src/containers/Storage/Storage.js
+++ src/containers/Storage/Storage.js
@@ -2,16 +2,19 @@ import React from "react";
 import './Storage.css'
 import Options from '../Options/Options';
 import { Button } from 'antd';
+import SideBar from "../SideBar/SideBar"
 
 function Storage(){
     return(
         <div className="body">
-            <Options>
-                <Button className="option" type="primary" shape="circle">Search</Button>
-                <Button className="option" type="primary" shape="circle">Add</Button>
-                <Button className="option" type="primary" shape="circle">Sort</Button>
-                <Button className="option" type="primary" shape="circle">Filter</Button>
-            </Options>
+            <SideBar>
+                <Options>
+                    <Button className="option" type="primary" shape="circle">Search</Button>
+                    <Button className="option" type="primary" shape="circle">Add</Button>
+                    <Button className="option" type="primary" shape="circle">Sort</Button>
+                    <Button className="option" type="primary" shape="circle">Filter</Button>
+                </Options>
+            </SideBar>
             <div className="content">
                 <h1>Storage</h1>
             </div>
